82e5f0b40f27ff540e53fedf6d8c313124652d27
loisMac <155264617+loisMac@users.noreply.github.com>
2024-11-07 12:50:06 +0000

    Made basic files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import "./styles.css";
 import App from './App';
+import axios from 'axios';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
