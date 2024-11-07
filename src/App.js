4c5d7c2efb912893fc4760d5fb9e97bae9d3f903
loisMac <155264617+loisMac@users.noreply.github.com>
2024-11-07 18:35:10 +0000

    Added github link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 
+import { NavLink } from 'react-bootstrap';
 import Weather from './Weather.js';
+import Button from './Button.js';
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
+      <Button />
     </div>
+    
   );
 }
+
 
---
+++ src/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = () => {
+    return (
+      <a href="https://github.com/loisMac/react-weather" target="_blank" rel="noopener noreferrer">
+        <button>GitHub</button>
+      </a>
+    );
+  }
+  
+  export default Button;
